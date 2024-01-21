ebac13bba61065c6d9a613f2aed3c76e04204639
Fernando Gonzalez <fernandogonzalezvaldz@gmail.com>
2024-01-21 12:50:55 -0300

    resolved

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -11,22 +11,45 @@ const profilePicture = "./images/me.jpg"
 /* Step 3 - Element Variables */
 
 const nameElement = document.getElementById("name");
-const foodElement = nameElement.getElementById("food");
+const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
-let img = document.getElementsByTagName("img");
+let imageElement = document.querySelector('#home picture img');
+
+// Ahora, imageElement contiene una referencia al elemento de la imagen.
+
 
 /* Step 4 - Adding Content */
 
 
 nameElement.innerHTML = `<strong>${fullName}<strong/>`;
 yearElement.textContent = currentYear;
-img.setAttribute("src", profilePicture);
+imageElement.setAttribute("src", profilePicture);
 
 
 /* Step 5 - Array */
 
 
-const food = [];
+const foods = ['Empanadas',
+    'Cazuela',
+    'Completo',
+    'Pastel de Choclo',
+    'Humitas',
+    'Arepas',
+    'Pabellón Criollo',
+    'Hallaca',
+    'Tequeños',
+    'Cachapa'];
 
 
+const favoriteFood = "Pizza";
 
+foods.push(favoriteFood);
+foodElement.innerHTML += `<br>${foods}`;
+// foods.forEach(food => {
+//     foodElement.innerHTML += `<br>${food}`;
+// });
+
+foods.shift();
+foodElement.innerHTML += `<br>${foods}`;
+foods.pop();
+foodElement.innerHTML += `<br>${foods}`;
